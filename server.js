7e6db6232ac8eca23914478ca15b0cd091dd8b76
Harry Sun <sunnyhay1@gmail.com>
2018-02-17 11:18:09 -0800

    mongodb

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", function (req, res) {
-  res.send("Hello World");
+  res.send("Hello Jerry Sun");
 });
 
 const server = app.listen(8081, function () {
