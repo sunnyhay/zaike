8f85ca49df2f3939ec0f99cbc1293636d4bd0877
Harry Sun <sunnyhay1@gmail.com>
2018-02-26 19:59:19 -0800

    simple progress

--- server.js
+++ server.js
@@ -1,16 +1,44 @@
 const express = require("express");
-//import fs from "fs";
+const cookieParser = require("cookie-parser");
 
 const app = express();
+app.use(cookieParser());
+
 
 app.get("/", function (req, res) {
   res.send("Hello Jerry Sun");
+  console.log("Cookies: ", req.cookies);
+
 });
 
-const server = app.listen(8081, function () {
-  const host = server.address().address;
-  const port = server.address().port;
-
-  console.info("Example app listening at http://%s:%s", host, port);
+// This responds a POST request for the homepage
+app.post("/", function (req, res) {
+  console.log("Got a POST request for the homepage");
+  res.send("Hello POST");
 });
 
+// This responds a DELETE request for the /del_user page.
+app.delete("/del_user", function (req, res) {
+  console.log("Got a DELETE request for /del_user");
+  res.send("Hello DELETE");
+});
+
+// This responds a GET request for the /list_user page.
+app.get("/list_user", function (req, res) {
+  console.log("Got a GET request for /list_user");
+  res.send("Page Listing");
+});
+
+// This responds a GET request for abcd, abxcd, ab123cd, and so on
+app.get("/ab*cd", function(req, res) {
+  console.log("Got a GET request for /ab*cd");
+  res.send("Page Pattern Match");
+});
+
+let server = app.listen(8081, function () {
+
+  let host = server.address().address;
+  let port = server.address().port;
+
+  console.log("Example app listening at http://%s:%s", host, port);
+});
---
+++ try-mongoose.js
@@ -0,0 +1,30 @@
+// getting-started.js
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/test");
+
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function() {
+  console.info("connected!");
+  var kittySchema = mongoose.Schema({
+    name: String
+  });
+  // NOTE: methods must be added to the schema before compiling it with mongoose.model()
+  kittySchema.methods.speak = function () {
+    var greeting = this.name
+      ? "Meow name is " + this.name
+      : "I don't have a name";
+    console.log(greeting);
+  };
+
+  var Kitten = mongoose.model("Kitten", kittySchema);
+  var silence = new Kitten({ name: "Silence" });
+  silence.save(function (err, silence) {
+    if (err) return console.error(err);
+    silence.speak();
+    console.log(silence.name); // 'Silence'
+    silence.speak();
+    db.close();
+  });
+
+});
