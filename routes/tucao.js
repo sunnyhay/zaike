1fcb75c84a5a2e93adf41f6b2ab15f55a2f922ea
Harry Sun <sunnyhay1@gmail.com>
2018-03-25 11:53:26 -0700

    insert tucao

--- routes/tucao.js
+++ routes/tucao.js
@@ -3,6 +3,10 @@
 const express = require("express");
 const router = express.Router();
 
+// own lib
+const logger = require("../lib/logger");
+const log = logger.getLogger("tucao");
+
 // configuration
 const config = require("../config/config.json");
 // target table
@@ -11,22 +15,39 @@ const tucao_table = config.dev_env.db.tucao_table;
 module.exports = function(db) {
   // middleware that is specific to this router
   // router.use(function (req, res, next) {
-  //   console.log("Time: ", Date.now());
+  //   log.log("Time: ", Date.now());
   //   next();
   // });
+  const collection = db.collection(tucao_table);
 
-  router.get("/", function (req, res) {
-    const collection = db.collection(tucao_table);
+  router.get("/", (req, res) => {
     // Find some documents
     collection.find({}).toArray(function (err, docs) {
-      console.info("Found the following records");
-      console.info(docs);
+      log.info("Found the following records");
+      log.info(docs);
       res.send(docs);
     });
   });
 
+  /*
+  吐槽entry有ID，用户ID，用户昵称，城市ID，城市名称，景区ID，景区名称，总体宰客评分，吃住行购物其他这五方面的分数，
+  正文，点赞数目，回复留言数组包含所有针对该吐槽的留言，创建时间，修改时间，删除软标志，重要等级（人工设置辅助）
+  */
+  router.post("/", (req, res) => {
+    const record = req.body;
+    record.created = new Date();
+    record.modified = new Date();
+    log.info(record);
+
+    collection.insertOne(record, (err, r) => {
+      if (err) res.send(err);
+      log.info(r);
+      res.send(r);
+    });
+  });
+
   router.get("/:id", function (req, res) {
-    console.info(req.params.id);
+    log.info(req.params.id);
     res.send("Get ID: " + req.params.id);
   });
 
